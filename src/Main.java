8487d4d1d1d8c03b7ec32210f5ffa7d713ca8684
Jorge Andres Cerquera Ortiz <jorgeandresc05@gmail.com>
2017-04-03 10:11:51 -0500

    Initial sales of tools.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		Main.displayGreeting();
+		data.display();
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("HELLO HAPPY SALES PEOPLE");
+		System.out.println("THIS APP SHOW SALES DATA");
+	}
+}
