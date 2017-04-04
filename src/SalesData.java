23b59ee4e8a426a6d709a8481a8d15554dd36131
Jorge Andres Cerquera Ortiz <jorgeandresc05@gmail.com>
2017-04-04 11:20:56 -0500

    Completed displaying data in saleData object.

--- src/SalesData.java
+++ src/SalesData.java
@@ -3,8 +3,8 @@ public class SalesData {
 	int[] data = {0, 4, 42};
 	
 	public void display() {
+		System.out.println("Data: ");
 		for(int i = 0; i < data.length; i++){
-			System.out.println("Data: ");
 			System.out.println("Next value: " + data[i]);
 		}
 	}
